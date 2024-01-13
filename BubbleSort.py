6451da27fd061888db5a4d81a2851d26b9a67f64
Dizzydoze <overdosedizzy@gmail.com>
2024-01-12 17:37:03 -0800

    InsertionSort and BubbleSort completed and test passed.

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,4 +1,6 @@
 def bubble_sort(arr):
+    # i rounds for i elements in the arr
+    # worst case we have to go i rounds for each of the num
     for i in range(len(arr)):
         # last i nums must have been sorted
         for j in range(len(arr) - i - 1):
@@ -8,10 +10,25 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
+# using flag to stop when sorting is finished
+def bubble_sort_flag(arr):
+    swap = True
+    while swap:
+        # in the beginning of every round, NO swap happened
+        swap = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swap = True
+    # no swap happened, exit the loop
+
+
+
 if __name__ == '__main__':
     import random
     arr = [random.randint(0, 100) for _ in range(10)]
     print(arr)
-    bubble_sort(arr)
+    # bubble_sort(arr)
+    bubble_sort_flag(arr)
     print(arr)
 
--- InsertionSort.py
+++ InsertionSort.py
@@ -1,27 +1,22 @@
 def insertionSort(arr):
     """
-    check backward, j bigger, move it forward.
+    moving smaller element to the LEFT
+    check backward, num at j smaller, move it to the LEFT.
     j<-j<-j<-j<-j  i
     7->8->2->3->9->1->0->4->6
     in-place, nothing return
     """
-    for i in range(1, len(arr)):      # start from 1, leave a spot for j
-        # save current element for later insertion after spot found
-        cur = arr[i]
-        # j be the backward index helping to find the insertion spot
-        j = i - 1
-        while j >= 0 and arr[j] > cur:
-            # j is bigger, move it one step forward
-            arr[j+1] = arr[j]
-            # move j backward
-            j -= 1
-        # spot found, insert the element we save earlier
-        arr[j+1] = cur
+    for i in range(1, len(arr)):
+        # start from current index i, moving backward, to index 0, exclusive since j-1==0
+        for j in range(i, 0, -1):
+            # j smaller, move it to the LEFT
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
 
 
 if __name__ == '__main__':
     import random
-    arr = [random.randint(1, 100) for _ in range(20)]
+    arr = [random.randint(1, 100) for _ in range(10)]
     print("Before: ", arr)
     insertionSort(arr)
     print("After: ", arr)
